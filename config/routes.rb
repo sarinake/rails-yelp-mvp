80a3bcd019cce45e023eb750af749e26bb9b92f1
Sarina Ke <sarina.y.ke@gmail.com>
2022-08-18 16:09:36 +0800

    finished yelp MVP challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # if we don't put reviews in restaurants, we won't be able to do restaurants/.../reviews
+  # we would only be able to do restaurants/... and reviews/...
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
